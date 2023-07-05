5730bb402fbe65b19b64c34a653fbed5e4f3370f
NaiM-AyouB <ayoubnaim833@gmail.com>
2023-07-05 04:29:56 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+trlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+
+	}
+	return (i);
+}
