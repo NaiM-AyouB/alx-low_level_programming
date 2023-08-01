eb1087095e545e81fd39ab2e751a9dcc0bef0f20
NaiM-AyouB <ayoubnaim833@gmail.com>
2023-08-01 03:28:57 -0700

    update the tasks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,11 +3,8 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list.
- *
  * @h: head of linklist node
- *
  * Return: the number of nodes
- *
  */
 size_t print_listint(const listint_t *h)
 {
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,11 +2,8 @@
 
 /**
  * listint_len - the number of elements in a linked listint_t list.
- *
  * @h: head pointer
- *
  * Return: the number of elements
- *
  */
 
 size_t listint_len(const listint_t *h)
