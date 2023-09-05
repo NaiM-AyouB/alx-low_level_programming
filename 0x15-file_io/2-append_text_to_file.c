ee196ad5f92db9b0613fa5a21a1ea4da87f7997e
NaiM-AyouB <ayoubnaim833@gmal.com>
2023-09-04 18:18:49 -0700

    tasks

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (len == -1)
+		return (-1);
+	return (1);
+}
